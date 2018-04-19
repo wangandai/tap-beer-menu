eed897d89b62f6e471d2db8125dbb2a6a82498a2
Wang Andai <wang.andai@gmail.com>
2018-04-19 18:27:20 +0800

    Changed server file to main

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-import menu
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+import logging
+import telegram_bot_handlers as handlers
 
-m = menu.get_today_menu()
-go_today, beers = menu.is_worth_going(m)
 
-print(go_today)
-print(beers)
+def init_handlers(dispatcher):
+    dispatcher.add_handler(CommandHandler('start', handlers.start))
+    dispatcher.add_handler(CommandHandler('menu', handlers.get_menu))
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    bot_token = '561594972:AAFn1qhEl9gaXjH7Y2nvggYt4prJbyKU7V4'
+    updater = Updater(token=bot_token)
+    dispatcher = updater.dispatcher
+
+    init_handlers(dispatcher)
+
+    updater.start_polling()
+
+
+if __name__ == "__main__":
+    main()
