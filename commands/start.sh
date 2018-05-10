dc0ae1d7d4bde56945edfc7d3cd8a342dc980172
Wang Andai <wang.andai@gmail.com>
2018-05-10 14:25:40 +0800

    Added nohup

--- commands/start.sh
+++ commands/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pipenv run python3 menu_server.py & disown
+nohup pipenv run python3 menu_server.py & disown
 sleep 15
-pipenv run python3 telegram_listener.py & disown
+nohup pipenv run python3 telegram_listener.py & disown
